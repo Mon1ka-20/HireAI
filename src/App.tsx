30a0d27f2ffef77f42e07b6631ba2de3c2257f71
Monika Jain <jainmonika386@gmail.com>
2025-05-31 01:24:03 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster as AppToaster } from "@/components/ui/toaster";
+import { Toaster as SonnerToaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -15,8 +14,10 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <AuthProvider>
-        <Toaster />
-        <Sonner />
+        {/* Toast notifications */}
+        <AppToaster />
+        <SonnerToaster />
+
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
